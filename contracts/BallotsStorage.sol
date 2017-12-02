99cb9d7335b6a9197d34de4b25c8231892ffe603
viktor <baranov.viktor.27@gmail.com>
2017-12-02 12:04:44 -0800

    Solhint BallotsStorage indentions suggestions

--- contracts/BallotsStorage.sol
+++ contracts/BallotsStorage.sol
@@ -1,19 +1,20 @@
 pragma solidity ^0.4.18;
 
+
 contract BallotsStorage {
-  address public votingToChangeThreshold;
-  uint256 public minThresholdOfVoters = 3;
+    address public votingToChangeThreshold;
+    uint256 public minThresholdOfVoters = 3;
 
-  modifier onlyVotingToChangeThreshold() {
-    require(msg.sender == votingToChangeThreshold);
-    _;
-  }
-  function BallotsStorage(address _votingToChangeThreshold) public {
-    votingToChangeThreshold = _votingToChangeThreshold;
-  }
+    modifier onlyVotingToChangeThreshold() {
+        require(msg.sender == votingToChangeThreshold);
+        _;
+    }
+    function BallotsStorage(address _votingToChangeThreshold) public {
+        votingToChangeThreshold = _votingToChangeThreshold;
+    }
 
-  function setMinThresholdOfVoters(uint256 _newValue) public onlyVotingToChangeThreshold {
-    require(_newValue > 0 && _newValue != minThresholdOfVoters);
-    minThresholdOfVoters = _newValue;
-  }
+    function setMinThresholdOfVoters(uint256 _newValue) public onlyVotingToChangeThreshold {
+        require(_newValue > 0 && _newValue != minThresholdOfVoters);
+        minThresholdOfVoters = _newValue;
+    }
 }
