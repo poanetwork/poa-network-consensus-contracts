532f9d267c520ef93cfa1aa902afbe5b081b5e06
Roman Storm & Viktor Baranov <rstormsf@gmail.com>
2017-12-05 21:24:51 -0800

    add missing dependency mochawesome

--- truffle.js
+++ truffle.js
@@ -2,6 +2,12 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 6600000,
+      network_id: "*" // Match any network id
+    },
     test: {
       host: "localhost",
       port: 8544,
