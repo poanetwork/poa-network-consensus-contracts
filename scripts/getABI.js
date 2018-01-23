28196de8ecc4e0f6681c8f22fa3afe1c3cf9205e
Victor Baranov <baranov.viktor.27@gmail.com>
2018-01-23 19:38:23 +0300

    (Feature) script to generate multi-line ABIs of contracts (#42)
    * script to generate multi-line ABIs of contracts
    
    * Improve getABI.js specify output dir

---
+++ scripts/getABI.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+const OUTPUT_DIR = process.env.OUTPUT || '../build/abis';
+if (!fs.existsSync(OUTPUT_DIR)) {
+  // Do something
+  fs.mkdirSync(OUTPUT_DIR);
+}
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
+}
+
+let dir = '../build/contracts/';
+readFiles(dir, function(filename, content) {
+	if (filename.includes(".abi.")) return;
+	let abi = JSON.stringify(JSON.parse(content).abi, null, 2);
+	fs.writeFileSync(`${OUTPUT_DIR}${filename}.abi.json`, abi);
+}, function(err) {
+  throw err;
+});
