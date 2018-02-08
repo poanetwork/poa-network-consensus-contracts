814d65af922f89e9bbe928c6cdec9a86b1fd00eb
viktor <baranov.viktor.27@gmail.com>
2018-02-08 20:13:37 +0300

    save to file contracts addresses

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var PoaNetworkConsensus = artifacts.require("./PoaNetworkConsensus.sol");
 var ProxyStorage = artifacts.require("./ProxyStorage.sol");
 var KeysManager = artifacts.require("./KeysManager.sol");
@@ -39,6 +40,21 @@ module.exports = async function(deployer, network, accounts) {
         VotingToChangeProxyAddress.address,
         BallotsStorage.address)
       await poaNetworkConsensus.setProxyStorage(ProxyStorage.address);
+
+      if (!!process.env.SAVE_TO_FILE === true) {
+        let contracts = {
+          "VOTING_TO_CHANGE_KEYS_ADDRESS": VotingToChangeKeys.address,
+          "VOTING_TO_CHANGE_MIN_THRESHOLD_ADDRESS": VotingToChangeMinThreshold.address,
+          "VOTING_TO_CHANGE_PROXY_ADDRESS": VotingToChangeProxyAddress.address,
+          "BALLOTS_STORAGE_ADDRESS": BallotsStorage.address,
+          "KEYS_MANAGER_ADDRESS": KeysManager.address,
+          "METADATA_ADDRESS": ValidatorMetadata.address,
+          "PROXY_ADDRESS": ProxyStorage.address
+        }
+
+        await saveToFile('./contracts.json', JSON.stringify(contracts, null, 2));
+      }
+
       console.log('Done')
       console.log('ADDRESSES:\n', 
      `VotingToChangeKeys.address ${VotingToChangeKeys.address} \n
@@ -57,5 +73,15 @@ module.exports = async function(deployer, network, accounts) {
   }
 };
 
-// POA_NETWORK_CONSENSUS_ADDRESS=0x8bf38d4764929064f2d4d3a56520a76ab3df415b MASTER_OF_CEREMONY=0xCf260eA317555637C55F70e55dbA8D5ad8414Cb0 OLD_KEYSMANAGER=0xfc90125492e58dbfe80c0bfb6a2a759c4f703ca8 ./node_modules/.bin/truffle migrate --reset --network sokol
-// DEPLOY_POA=true POA_NETWORK_CONSENSUS_ADDRESS=0x8bf38d4764929064f2d4d3a56520a76ab3df415b MASTER_OF_CEREMONY=0xCf260eA317555637C55F70e55dbA8D5ad8414Cb0 OLD_KEYSMANAGER=0xfc90125492e58dbfe80c0bfb6a2a759c4f703ca8 ./node_modules/.bin/truffle migrate --reset --network sokol
+function saveToFile(filename, content) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, content, (err) => {
+      console.log(err)
+      if (err) reject(err);
+      resolve();
+    });
+  });
+}
+
+// SAVE_TO_FILE=true POA_NETWORK_CONSENSUS_ADDRESS=0x8bf38d4764929064f2d4d3a56520a76ab3df415b MASTER_OF_CEREMONY=0xCf260eA317555637C55F70e55dbA8D5ad8414Cb0 OLD_KEYSMANAGER=0xfc90125492e58dbfe80c0bfb6a2a759c4f703ca8 ./node_modules/.bin/truffle migrate --reset --network sokol
+// SAVE_TO_FILE=true DEPLOY_POA=true POA_NETWORK_CONSENSUS_ADDRESS=0x8bf38d4764929064f2d4d3a56520a76ab3df415b MASTER_OF_CEREMONY=0xCf260eA317555637C55F70e55dbA8D5ad8414Cb0 OLD_KEYSMANAGER=0xfc90125492e58dbfe80c0bfb6a2a759c4f703ca8 ./node_modules/.bin/truffle migrate --reset --network sokol
