561351b10c14536bcc8fcd77590f07d1a5421985
varasev <33550681+vowme-vadim@users.noreply.github.com>
2018-03-24 18:08:31 +0300

    Trigger Travis build

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 8545, 
       gas: 6600000,
       network_id: "*" // Match any network id
     },
