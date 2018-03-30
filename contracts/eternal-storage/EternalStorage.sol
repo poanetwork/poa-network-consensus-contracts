7578508c59dbdd7c84d169ced190c1d568e612fb
varasev <33550681+varasev@users.noreply.github.com>
2018-03-30 20:00:29 +0300

    solhint fixes

--- contracts/eternal-storage/EternalStorage.sol
+++ contracts/eternal-storage/EternalStorage.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+
 /**
  * @title EternalStorage
  * @dev This contract holds all the necessary state variables to carry out the storage of any contract
@@ -7,43 +8,43 @@ pragma solidity ^0.4.18;
  */
 contract EternalStorage {
 
-  // Version number of the current implementation
-  uint256 internal _version;
+    // Version number of the current implementation
+    uint256 internal _version;
 
-  // Address of the current implementation
-  address internal _implementation;
+    // Address of the current implementation
+    address internal _implementation;
 
-  // Storage mappings
-  mapping(bytes32 => uint256) internal uintStorage;
-  mapping(bytes32 => string) internal stringStorage;
-  mapping(bytes32 => address) internal addressStorage;
-  mapping(bytes32 => bytes) internal bytesStorage;
-  mapping(bytes32 => bool) internal boolStorage;
-  mapping(bytes32 => int256) internal intStorage;
-  mapping(bytes32 => bytes32) internal bytes32Storage;
+    // Storage mappings
+    mapping(bytes32 => uint256) internal uintStorage;
+    mapping(bytes32 => string) internal stringStorage;
+    mapping(bytes32 => address) internal addressStorage;
+    mapping(bytes32 => bytes) internal bytesStorage;
+    mapping(bytes32 => bool) internal boolStorage;
+    mapping(bytes32 => int256) internal intStorage;
+    mapping(bytes32 => bytes32) internal bytes32Storage;
 
-  mapping(bytes32 => uint256[]) internal uintArrayStorage;
-  mapping(bytes32 => string[]) internal stringArrayStorage;
-  mapping(bytes32 => address[]) internal addressArrayStorage;
-  mapping(bytes32 => bytes[]) internal bytesArrayStorage;
-  mapping(bytes32 => bool[]) internal boolArrayStorage;
-  mapping(bytes32 => int256[]) internal intArrayStorage;
-  mapping(bytes32 => bytes32[]) internal bytes32ArrayStorage;
+    mapping(bytes32 => uint256[]) internal uintArrayStorage;
+    mapping(bytes32 => string[]) internal stringArrayStorage;
+    mapping(bytes32 => address[]) internal addressArrayStorage;
+    //mapping(bytes32 => bytes[]) internal bytesArrayStorage;
+    mapping(bytes32 => bool[]) internal boolArrayStorage;
+    mapping(bytes32 => int256[]) internal intArrayStorage;
+    mapping(bytes32 => bytes32[]) internal bytes32ArrayStorage;
 
-  /**
-  * @dev Tells the version number of the current implementation
-  * @return uint representing the number of the current version
-  */
-  function version() public view returns (uint256) {
-    return _version;
-  }
+    /**
+    * @dev Tells the version number of the current implementation
+    * @return uint representing the number of the current version
+    */
+    function version() public view returns (uint256) {
+        return _version;
+    }
 
-  /**
-  * @dev Tells the address of the current implementation
-  * @return address of the current implementation
-  */
-  function implementation() public view returns (address) {
-    return _implementation;
-  }
+    /**
+    * @dev Tells the address of the current implementation
+    * @return address of the current implementation
+    */
+    function implementation() public view returns (address) {
+        return _implementation;
+    }
 
 }
--- contracts/eternal-storage/EternalStorageProxy.sol
+++ contracts/eternal-storage/EternalStorageProxy.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.18;
 
-import './EternalStorage.sol';
-import '../interfaces/IEternalStorageProxy.sol';
+import "./EternalStorage.sol";
+import "../interfaces/IEternalStorageProxy.sol";
+
 
 /**
  * @title EternalStorageProxy
@@ -11,75 +12,59 @@ import '../interfaces/IEternalStorageProxy.sol';
  */
 contract EternalStorageProxy is EternalStorage, IEternalStorageProxy {
 
-  /**
-  * @dev This event will be emitted every time the implementation gets upgraded
-  * @param version representing the version number of the upgraded implementation
-  * @param implementation representing the address of the upgraded implementation
-  */
-  event Upgraded(uint256 version, address indexed implementation);
+    /**
+    * @dev This event will be emitted every time the implementation gets upgraded
+    * @param version representing the version number of the upgraded implementation
+    * @param implementation representing the address of the upgraded implementation
+    */
+    event Upgraded(uint256 version, address indexed implementation);
 
-  modifier onlyProxyStorage() {
-    require(msg.sender == addressStorage[keccak256("proxyStorage")]);
-    _;
-  }
-
-  function EternalStorageProxy(address _proxyStorage, address _implementationAddress) public {
-    addressStorage[keccak256("proxyStorage")] = _proxyStorage;
-    _implementation = _implementationAddress;
-  }
-
-  /**
-   * @dev Allows ProxyStorage contract to upgrade the current implementation.
-   * @param implementation representing the address of the new implementation to be set.
-   */
-  function upgradeTo(address implementation) public onlyProxyStorage {
-    require(_implementation != implementation);
-
-    uint256 _newVersion = _version + 1;
-    assert(_newVersion > _version);
-    _version = _newVersion;
-
-    _implementation = implementation;
-    Upgraded(_version, _implementation);
-  }
-
-  /**
-   * @dev Allows ProxyStorage contract to upgrade the current implementation and call the new implementation
-   * to initialize whatever is needed through a low level call.
-   * @param implementation representing the address of the new implementation to be set.
-   * @param data represents the msg.data to bet sent in the low level call. This parameter may include the function
-   * signature of the implementation to be called with the needed payload
-   */
-  function upgradeToAndCall(address implementation, bytes data)
-    payable
-    public
-    onlyProxyStorage
-  {
-    upgradeTo(implementation);
-    require(this.call.value(msg.value)(data));
-  }
-
-  /**
-  * @dev Fallback function allowing to perform a delegatecall to the given implementation.
-  * This function will return whatever the implementation call returns
-  */
-  function () payable public {
-    address _impl = _implementation;
-
-    require(_impl != address(0));
-    bytes memory data = msg.data;
-
-    assembly {
-      let result := delegatecall(gas, _impl, add(data, 0x20), mload(data), 0, 0)
-      let size := returndatasize
-
-      let ptr := mload(0x40)
-      returndatacopy(ptr, 0, size)
-
-      switch result
-      case 0 { revert(ptr, size) }
-      default { return(ptr, size) }
+    modifier onlyProxyStorage() {
+        require(msg.sender == addressStorage[keccak256("proxyStorage")]);
+        _;
     }
-  }
+
+    function EternalStorageProxy(address _proxyStorage, address _implementationAddress) public {
+        addressStorage[keccak256("proxyStorage")] = _proxyStorage;
+        _implementation = _implementationAddress;
+    }
+
+    /**
+    * @dev Fallback function allowing to perform a delegatecall to the given implementation.
+    * This function will return whatever the implementation call returns
+    */
+    function () public payable {
+        address _impl = _implementation;
+
+        require(_impl != address(0));
+        bytes memory data = msg.data;
+
+        assembly {
+            let result := delegatecall(gas, _impl, add(data, 0x20), mload(data), 0, 0)
+            let size := returndatasize
+
+            let ptr := mload(0x40)
+            returndatacopy(ptr, 0, size)
+
+            switch result
+            case 0 { revert(ptr, size) }
+            default { return(ptr, size) }
+        }
+    }
+
+    /**
+     * @dev Allows ProxyStorage contract to upgrade the current implementation.
+     * @param implementation representing the address of the new implementation to be set.
+     */
+    function upgradeTo(address implementation) public onlyProxyStorage {
+        require(_implementation != implementation);
+
+        uint256 _newVersion = _version + 1;
+        assert(_newVersion > _version);
+        _version = _newVersion;
+
+        _implementation = implementation;
+        Upgraded(_version, _implementation);
+    }
 
 }
